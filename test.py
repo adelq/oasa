fc9ab1aababfed68cc69880acd383e55d0557d2b
Beda Kosata <beda@zirael.org>
2009-01-29 15:42:56 +0000

    release of 0.12.6
    git-archimport-id: beda@zirael.org--2004/oasa--develop--0.1.0--patch-191

--- test.py
+++ test.py
@@ -2,14 +2,20 @@ import oasa
 #print oasa.CAIRO_AVAILABLE
 
 def cairo_out_test2():
-    #mol = smiles.text_to_mol( "COC(=O)CNC(C1=CC=CC=C1)C2=C(C=CC(=C2)Br)NC(=O)C3=CC(=CC=C3)Cl")
-    #mol = smiles.text_to_mol( "c1nccc2c1cncn2")
-    mol = oasa.smiles.text_to_mol( "OC(O)C(O)C(O)CO")
+    mol = oasa.smiles.text_to_mol( "c1ccccc1Cl.c1ccccc1OC.CCCl")
     mol.normalize_bond_length( 30)
     mol.remove_unimportant_hydrogens()
     c = oasa.cairo_out.cairo_out( color_bonds=True, color_atoms=True)
     c.show_hydrogens_on_hetero = True
     c.font_size = 20
-    c.mol_to_cairo( mol, "test.png")
+    mols = list( mol.get_disconnected_subgraphs())
+    c.mols_to_cairo( mols, "test.pdf", format="pdf")
+    c.mols_to_cairo( mols, "test.png")
+    c.mols_to_cairo( mols, "test.svg", format="svg")
+
+def inchi_test():
+    mol = oasa.smiles.text_to_mol( "c1ccccc1\C=C/CC")
+    print oasa.inchi.mol_to_text( mol, program="stdinchi-1.exe", fixed_hs=False)
 
 cairo_out_test2()
+#inchi_test()
