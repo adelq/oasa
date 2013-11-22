c065cdffd513a6efe0149fc4b4477227604f67d6
Reinis Danne <rei4dan@gmail.com>
2013-11-22 20:43:30 +0200

    geometry.py: Unpack function parameters

--- oasa/geometry.py
+++ oasa/geometry.py
@@ -195,7 +195,7 @@ def point_distance( x1, y1, x2, y2):
   return sqrt( (x2-x1)**2 + (y2-y1)**2)
 
 
-def line_length( (x1,y1,x2,y2)):
+def line_length(x1, y1, x2, y2):
   return point_distance( x1, y1, x2, y2)
 
 
