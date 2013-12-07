3977aa8b308f280fb2acf26f5dab51ed8c4db8d7
Reinis Danne <rei4dan@gmail.com>
2013-12-07 21:11:57 +0200

    graph/basic.py: Use new style classes

--- oasa/graph/basic.py
+++ oasa/graph/basic.py
@@ -22,17 +22,21 @@
 #--------------------------------------------------------------------------
 
 
-class attribute_flexible_class:
-  """this class provides mechanism for addition and removing of attributes on fly"""
 
+class attribute_flexible_class(object):
+  """Provide mechanism for addition and removing of attributes on fly.
+
+  """
   def __init__( self):
     pass
+
 
   def add_attribute( self, name, value=None):
     if name not in self.__dict__:
       self.__dict__[ name] = value
       return 1
     return 0
+
 
   def del_attribute( self, name):
     if name not in self.__dict__:
