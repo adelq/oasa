aecb48ff1e8438bbf619176d0c82d9745d099091
Reinis Danne <rei4dan@gmail.com>
2013-12-07 21:19:31 +0200

    graph/edge.py: Use decorators

--- oasa/graph/edge.py
+++ oasa/graph/edge.py
@@ -33,8 +33,10 @@ class edge(object):
     self.properties_ = {}
     self.disconnected = False
 
+
   def __str__( self):
     return "edge between %s %s" % tuple( map( str, self.vertices))
+
 
   def copy( self):
     other = self.__class__()
@@ -42,12 +44,15 @@ class edge(object):
       setattr( other, attr, copy.copy( getattr( self, attr)))
     return other
 
+
   def set_vertices( self, vs=[]):
     if vs and len( vs) == 2:
       self.vertices = set( vs)
 
+
   def get_vertices( self):
     return self.vertices
+
 
   def get_neighbor_edges( self):
     v1, v2 = self.vertices
@@ -55,20 +60,21 @@ class edge(object):
     out2 = [e for e in v2.get_neighbor_edges() if e!=self]
     return out1 + out2
 
+
   def get_neighbor_edges2( self):
     """returns 2 lists of neighbor edges (one for one side, one for the other)"""
     v1, v2 = self.vertices
     out1 = [e for e in v1.get_neighbor_edges() if e!=self]
     out2 = [e for e in v2.get_neighbor_edges() if e!=self]
     return out1, out2
-    
-  def _get_disconnected( self):
+
+
+  @property
+  def disconnected(self):
     return self._disconnected
 
-  def _set_disconnected( self, d):
+
+  @disconnected.setter
+  def disconnected(self, d):
     self._disconnected = d
-   # if d:
-   #   warn(a "aaaaaa", UserWarning, 4)
 
-
-  disconnected = property( _get_disconnected, _set_disconnected)
