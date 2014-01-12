b692eb96c0388f9d1a3a36ecc1573775f7db4e45
Reinis Danne <rei4dan@gmail.com>
2014-01-12 17:17:19 +0200

    Prepare release 0.14.0b1

--- setup.py
+++ setup.py
@@ -1,29 +1,20 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
-import sys
 from distutils.core import setup
-import glob
-import os
-import operator
 
-# the setup itself
+setup(name             = 'oasa',
+      version          = '0.14.0b1',
+      description      = "OASA is a free cheminformatics library written in Python",
+      author           = "Beda Kosata",
+      author_email     = "beda@zirael.org",
+      maintainer       = "Reinis Danne",
+      maintainer_email = "rei4dan@gmail.com"
+      url              = "http://bkchem.zirael.org/oasa_en.html",
+      license          = "GNU GPL",
+      platforms        = ["Unix", "Windows", "hopefully other OSes able to run Python"],
 
-set = setup(
-  name = 'oasa',
-  version = '0.13.2',
-  description = "OASA is a free cheminformatics library written in Python",
-  author = "Beda Kosata",
-  author_email = "beda@zirael.org",
-  url = "http://bkchem.zirael.org/oasa_en.html",
-  license = "GNU GPL",
-  platforms = ["Unix", "Windows", "hopefully other OSes able to run Python"],
-  long_description = "OASA is a free cheminformatics library written in Python",
+      long_description = "OASA is a free cheminformatics library written in Python",
 
-  packages=[ 'oasa', 'oasa/graph'],
-
-  #data_files=[ ('oasa', glob.glob( 'oasa/templates/*.cdml')+glob.glob('templates/*.xml')),
-
-  #windows=['oasa'],
-  #options = {"py2exe": {"packages": ["encodings"]}}
-  )
+      packages=['oasa', 'oasa/graph'],
+     )
 
