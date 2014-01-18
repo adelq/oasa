15b80a11dca24f7cf7410eeb1bb873be7cffd6f1
Reinis Danne <rei4dan@gmail.com>
2014-01-18 18:10:13 +0200

    cairo_out.py: Use built-in sorted() instead of sort() method

--- oasa/cairo_out.py
+++ oasa/cairo_out.py
@@ -495,8 +495,7 @@ class cairo_out(object):
     # now we can compare the angles
     angles = [geometry.clockwise_angle_from_east( x1-x, y1-y) for x1,y1 in coords]
     angles.append( 2*math.pi + min( angles))
-    angles.sort()
-    angles.reverse()
+    angles = sorted(angles, reverse=True)
     diffs = misc.list_difference( angles)
     i = diffs.index( max( diffs))
     angle = (angles[i] +angles[i+1]) / 2
