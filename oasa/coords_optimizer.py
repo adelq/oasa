2faa8a3ae32a22b61eee42e41fe476a1b6327198
Reinis Danne <rei4dan@gmail.com>
2016-02-23 20:22:17 +0200

    coords_optimizer.py: Use range() instead of xrange()

--- oasa/coords_optimizer.py
+++ oasa/coords_optimizer.py
@@ -41,7 +41,7 @@ def gen_variations(items, n):
     if n==0:
       yield []
     else:
-      for i in xrange( len(items)-n+1):
+      for i in range(len(items)-n+1):
         for v in gen_variations(items[i+1:],n-1):
           yield [items[i]]+v
 
